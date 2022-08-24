c105ab264a61826aec7aabf550a11cb64fca34ec
yeonyello <108323280+yeonyello@users.noreply.github.com>
2022-08-24 09:54:08 +0900

    make CRUD

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
+
 
 const app = express();
 const logger = morgan("dev");
@@ -18,6 +18,4 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
-
-app.listen(PORT, handleListening);
+export default app;
